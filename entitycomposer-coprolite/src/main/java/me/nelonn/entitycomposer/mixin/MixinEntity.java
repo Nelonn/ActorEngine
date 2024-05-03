b4252e59ad97ac679d148d31da680170fcf16583
Nelonn <42481486+Nelonn@users.noreply.github.com>
2024-05-03 21:16:44 +0300

    fix: when removing root entity other components does not removed

--- entitycomposer-coprolite/src/main/java/me/nelonn/entitycomposer/mixin/MixinEntity.java
+++ entitycomposer-coprolite/src/main/java/me/nelonn/entitycomposer/mixin/MixinEntity.java
@@ -1,6 +1,7 @@
 package me.nelonn.entitycomposer.mixin;
 
 import net.minecraft.world.entity.Entity;
+import org.bukkit.event.entity.EntityRemoveEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Entity.class)
 public class MixinEntity {
 
-    @Inject(method = "setRemoved", at = @At(value = "TAIL"))
-    private void inject_setRemoved(Entity.RemovalReason reason, CallbackInfo ci) {
+    @Inject(method = "setRemoved(Lnet/minecraft/world/entity/Entity$RemovalReason;Lorg/bukkit/event/entity/EntityRemoveEvent$Cause;)V", at = @At(value = "INVOKE", target = "Lorg/bukkit/craftbukkit/v1_20_R3/event/CraftEventFactory;callEntityRemoveEvent(Lnet/minecraft/world/entity/Entity;Lorg/bukkit/event/entity/EntityRemoveEvent$Cause;)V"))
+    private void inject_setRemoved(Entity.RemovalReason reason, EntityRemoveEvent.Cause cause, CallbackInfo ci) {
         Entity cur = (Entity) (Object) this;
         if (cur instanceof SetRemovedHandler handler) {
             handler.handleSetRemoved(reason);
